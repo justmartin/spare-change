65e7596d6876891344a574fa35a0320806e15579
justmartin <justin.lamar.martin@gmail.com>
2016-12-29 15:12:22 -0800

    Testing api key access to etsy.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,11 @@
 class HomeController < ApplicationController
+  require 'json'
+
+  def index
+    uri = URI("https://openapi.etsy.com/v2/listings/active?api_key=hyd3pgxe41bbdxlbieh53xpa")
+    
+    response = Net::HTTP.get(uri)
+
+    @parsed = JSON.parse(response)
+  end
 end
