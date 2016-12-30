485b29662de0d0d8c4c7fce065f9a161895fbb50
justmartin <justin.lamar.martin@gmail.com>
2016-12-30 10:43:35 -0800

    Moved api call out of controller into model.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,7 @@
 class HomeController < ApplicationController
-  require 'json'
 
   def index
-    uri = URI("https://openapi.etsy.com/v2/listings/active?api_key=hyd3pgxe41bbdxlbieh53xpa")
-    
-    response = Net::HTTP.get(uri)
-
-    @parsed = JSON.parse(response)
+    @listings = GetEtsy.get_active_listings
   end
+  
 end
--- app/models/get_etsy.rb
+++ app/models/get_etsy.rb
@@ -1,2 +1,13 @@
+require "json"
+
 class GetEtsy < ApplicationRecord
+
+  def self.get_active_listings
+    uri = URI("https://openapi.etsy.com/v2/listings/active?api_key=hyd3pgxe41bbdxlbieh53xpa")
+    
+    response = Net::HTTP.get(uri)
+
+    @parsed = JSON.parse(response)
+  end
+  
 end
