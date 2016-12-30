b4209e9b45410eec38476c0aaa246aef83623a34
justmartin <justin.lamar.martin@gmail.com>
2016-12-30 11:22:24 -0800

    Filtered results to include requested fields for actice products.

--- app/models/get_etsy.rb
+++ app/models/get_etsy.rb
@@ -3,7 +3,7 @@ require "json"
 class GetEtsy < ApplicationRecord
 
   def self.get_active_listings
-    uri = URI("https://openapi.etsy.com/v2/listings/active?api_key=hyd3pgxe41bbdxlbieh53xpa")
+    uri = URI("https://openapi.etsy.com/v2/listings/active?api_key=hyd3pgxe41bbdxlbieh53xpa&fields=listing_id,state,category_id,title,description,price,currency_code,quantity,tags,category_path,materials,url,views,style,language")
     
     response = Net::HTTP.get(uri)
 
