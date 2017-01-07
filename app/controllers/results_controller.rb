caabe1d366fe39275ab6530f89fac0f5d36c3321
justmartin <justin.lamar.martin@gmail.com>
2017-01-06 17:31:41 -0800

    Added links to paginate through results.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -4,4 +4,16 @@ class ResultsController < ApplicationController
     @listings = GetEtsy.get_active_listings(params[:amount].to_f)
   end
 
+  def results_2
+    @listings = GetEtsy.get_active_listings_2(params[:amount].to_f)
+  end
+
+  def results_3
+    @listings = GetEtsy.get_active_listings_3(params[:amount].to_f)
+  end
+
+  def results_4
+    @listings = GetEtsy.get_active_listings_4(params[:amount].to_f)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "search#index"
   get "/search" => "results#index"
+  get "/results2" => "results#results_2"
+  get "/results3" => "results#results_3"
+  get "/results4" => "results#results_4"
 end
